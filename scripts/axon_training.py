a23406a617facde826a36b6f1ef771690a3ae17b
Armand Collin <armac@videotron.ca>
2023-08-21 11:57:20 -0400

    Fix bugs in validation and plot val_loss

--- scripts/axon_training.py
+++ scripts/axon_training.py
@@ -24,10 +24,11 @@ from utils import bids_utils
 
 
 datapath = Path('/home/GRAMES.POLYMTL.CA/arcol/data_axondeepseg_tem')
-derivatives_path = Path('/home/GRAMES.POLYMTL.CA/arcol/collin_project/scripts/derivatives')# checkpoint = '/home/GRAMES.POLYMTL.CA/arcol/sam_myelin_seg_TEM/scripts/sam_vit_b_01ec64.pth'
+derivatives_path = Path('/home/GRAMES.POLYMTL.CA/arcol/collin_project/scripts/derivatives')
+checkpoint = '/home/GRAMES.POLYMTL.CA/arcol/sam_myelin_seg_TEM/scripts/sam_vit_b_01ec64.pth'
 device = 'cuda:0'
 preprocessed_data_path = '/home/GRAMES.POLYMTL.CA/arcol/sam_myelin_seg_TEM/scripts/tem_split/train/'
-val_preprocessed_datapath = 'home/GRAMES.POLYMTL.CA/arcol/sam_myelin_seg_TEM/scripts/tem_split/val/'
+val_preprocessed_datapath = '/home/GRAMES.POLYMTL.CA/arcol/sam_myelin_seg_TEM/scripts/tem_split/val/'
 # datapath = Path('/home/herman/Documents/NEUROPOLY_21/datasets/data_axondeepseg_tem/')
 # derivatives_path = Path('/home/herman/Documents/NEUROPOLY_22/COURS_MAITRISE/GBM6953EE_brainhacks_school/collin_project/scripts/derivatives/')
 # checkpoint = '/home/herman/Documents/NEUROPOLY_22/COURS_MAITRISE/GBM6953EE_brainhacks_school/collin_project/scripts//sam_vit_b_01ec64.pth'
@@ -71,9 +72,13 @@ loss_fn = monai.losses.DiceLoss(sigmoid=True)
 
 
 # Training loop
-num_epochs = 10
-batch_size = 1
+num_epochs = 400
+batch_size = 4
 mean_epoch_losses = []
+mean_val_losses = []
+val_epochs = []
+val_frequency = 5
+
 transform = ResizeLongestSide(sam_model.image_encoder.img_size)
 train_dset = bids_utils.AxonDataset(preprocessed_data_path)
 train_dataloader = DataLoader(
@@ -87,7 +92,6 @@ val_dataloader = DataLoader(val_dset, batch_size=1)
 
 for epoch in range(num_epochs):
     epoch_losses = []
-    val_epochs = []
     val_losses = []
 
     sam_model.train()
@@ -137,12 +141,12 @@ for epoch in range(num_epochs):
         epoch_losses.append(loss.item())
         optimizer.step()
         optimizer.zero_grad()
-        break
     
     # validation loop every 5 epochs to avoid cluttering
     #TODO validation loop needs to be updated
-    if epoch % 5 == 0:
+    if epoch % val_frequency == 0:
         sam_model.eval()
+        val_epochs.append(epoch)
         for (val_imgs, val_gts, sizes, names) in val_dataloader:
             with torch.no_grad():
                 input_size = val_imgs.shape
@@ -175,26 +179,27 @@ for epoch in range(num_epochs):
             #TODO: compute, print, store validation loss (no grad)
             val_loss = loss_fn(upscaled_mask, gt_binary_mask)
             val_losses.append(val_loss.item())
-            val_epochs.append(epoch)
             #TODO: save validation img
     #         fname = emb_path.stem.replace('embedding', f'val-seg-axon_epoch{epoch}.png')
     #         plt.imsave(Path('axon_validation_results') / fname, binary_mask.cpu().detach().numpy().squeeze(), cmap='gray')
 
     mean_epoch_losses.append(np.mean(epoch_losses))
-    mean_val_losses.append(np.mean(val_losses))
-    print(f'EPOCH {epoch}\n\tMEAN LOSS: {mean_epoch_losses[-1]}\n\tMEAN VAL LOSS: {mean_val_losses[-1]}')
+    print(f'EPOCH {epoch}\n\tMEAN LOSS: {mean_epoch_losses[-1]}')
+    if epoch % val_frequency == 0:
+        mean_val_losses.append(np.mean(val_losses))
+        print(f'\tMEAN VAL LOSS: {mean_val_losses[-1]}')
     if epoch % 40 == 0:
-        torch.save(sam_model.state_dict(), f'sam_vit_b_01ec64_epoch_{epoch}_auto-axon-seg.pth')
-torch.save(sam_model.state_dict(), 'sam_vit_b_01ec64_finetuned_auto-axon-seg.pth')
+        torch.save(sam_model.state_dict(), f'sam_vit_b_01ec64_epoch_{epoch}_auto-axon-seg_run4.pth')
+torch.save(sam_model.state_dict(), 'sam_vit_b_01ec64_finetuned_auto-axon-seg_run4.pth')
 
 # Plot mean epoch losses
 
 plt.plot(list(range(len(mean_epoch_losses))), mean_epoch_losses)
+print(val_epochs)
+print(mean_val_losses)
 plt.plot(val_epochs, mean_val_losses)
 plt.legend(['Training loss', 'Validation loss'])
 plt.title('Mean epoch loss for axon segmentation')
 plt.xlabel('Epoch Number')
 plt.ylabel('Loss')
-plt.savefig('losses_axon_seg_vit_h.png')
-
-# Plot validation losses
+plt.savefig('losses_axon_seg_vit_b_run4.png')
