53f3e9d184b9d06cb09c8fe8885a0dd3130587d6
Armand Collin <armac@videotron.ca>
2023-09-06 13:11:44 -0400

    Support axon centroid prompting in segmentation script

--- scripts/utils/segment_axons.py
+++ scripts/utils/segment_axons.py
@@ -10,31 +10,43 @@ import cv2
 import torch
 from segment_anything import SamPredictor, sam_model_registry
 
+import bids_utils
+
+
 def get_predictor(model_type, checkpoint, device):
     sam_model = sam_model_registry[model_type](checkpoint=checkpoint)
     sam_model.to(device)
     return SamPredictor(sam_model)
-
 
 def main(args):
     model_type = args['model_type']
     checkpoint = args['checkpoint']
     image_path = args['img']
     device = args['device']
+    prompt_with_centroids = args['centroid_file'] != None
     
     predictor = get_predictor(model_type, checkpoint, device)
     image = cv2.imread(image_path)
     predictor.set_image(image)
 
-    # get full bbox prompt
-    H, W = predictor.original_size
-    prompt = np.array([[0,0,W-1,H-1]])
-
-    mask, _, _ = predictor.predict(
-        point_coords=None,
-        box=prompt,
-        multimask_output=False
-    )
+    if prompt_with_centroids:
+        points, labels = bids_utils.load_centroid_prompts([args['centroid_file']], device)
+        print("POINTS SHAPE -> ", points.shape)
+        mask, _, _ = predictor.predict(
+            point_coords=points,
+            point_labels=labels,
+            box=None,
+            multimask_output=False
+        )
+    else:
+        # get full bbox prompt
+        H, W = predictor.original_size
+        prompt = np.array([[0,0,W-1,H-1]])
+        mask, _, _ = predictor.predict(
+            point_coords=None,
+            box=prompt,
+            multimask_output=False
+        )
 
     fname = f'{Path(image_path).parent / Path(image_path).stem}_axonseg.png'
     cv2.imwrite(fname, mask[0] * 255)
@@ -48,8 +60,9 @@ if __name__ == "__main__":
     parser.add_argument("checkpoint", help="Model checkpoint")
 
     # Optional argument which requires a parameter (eg. -d test)
-    parser.add_argument("-m", "--model-type", default="vit_b", help="Model type")
+    parser.add_argument("-m", "--model_type", default="vit_b", help="Model type")
     parser.add_argument("-d", "--device", help="Torch device", default='cpu')
+    parser.add_argument("-c", "--centroid_file", help="Path to CSV file containing axon centroids.", default=None)
 
     args = parser.parse_args()
     main(vars(args))
