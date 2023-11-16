ba5179b3a645ec3e1bd31d81a6a308634b41f402
Armand Collin <armac@videotron.ca>
2023-11-16 16:35:35 -0500

    Let user chose seg class (axon or myelin) for dice computation

--- scripts/utils/compute_dice.py
+++ scripts/utils/compute_dice.py
@@ -5,27 +5,53 @@ import cv2
 import torch
 import argparse
 
+SUFFIX_MAPPING = {
+    'SAM': {
+        'AXON': '_TEM_axonseg.png',
+        'MYELIN': '_TEM_myelinseg.png'
+    },
+    'NNUNET': {
+        'AXON': '_TEM_segnn-axon.png',
+        'MYELIN': '_TEM_segnn-myelin.png'
+    },
+    'ADS': {
+        'AXON': '_TEM_seg-axon.png',
+        'MYELIN': '_TEM_seg-myelin.png'
+    },
+    'BIDS': {
+        'AXON': '_TEM_seg-axon-manual.png',
+        'MYELIN': '_TEM_seg-myelin-manual.png'
+    },
+}
+
 def get_parser():
     # parse command line arguments
     parser = argparse.ArgumentParser(description='Segment images using nnUNet')
-    parser.add_argument('--path-dataset', type=str, help='Path to original (BIDS) dataset.')
-    parser.add_argument('--path-preds', type=str, help='Path to predictions.')
-    parser.add_argument('--path-baseline-preds', type=str, help='Path to baseline IVADOMED predictions.')
-    parser.add_argument('--is-nnunet', default=False, action='store_true', help='if preds come from nnUNetv2 model')
+    parser.add_argument('--path-dataset', type=str, required=True, help='Path to original (BIDS) dataset.')
+    parser.add_argument('--path-preds', type=str, required=True, help='Path to predictions.')
+    parser.add_argument('--path-baseline-preds', required=True, type=str, help='Path to baseline IVADOMED predictions.')
+    parser.add_argument('--segtype', type=str, default='AXON', help='Target class; either AXON or MYELIN; defaults to AXON.')
+    parser.add_argument('--is-nnunet', default=False, action='store_true', help='if preds come from nnUNetv2 model. Defaults to false.')
 
     return parser
 
 def main():
     parser = get_parser()
     args = parser.parse_args()
+    
+    segtype = args.segtype.upper()
+    assert segtype in ['AXON', 'MYELIN'], 'Please provide a valid target class. Either AXON or MYELIN.'
 
     gt_base = str(Path(args.path_dataset) / 'derivatives' / 'labels' / 'sub-nyuMouse26' / 'micr' / 'sub-nyuMouse26_sample-000')
-    print(gt_base)
     fname_base = str(Path(args.path_preds) / 'sub-nyuMouse26_sample-000')
     ads_fname_base = str(Path(args.path_baseline_preds) / 'sub-nyuMouse26_sample-000')
     target_suffix = '_TEM_myelinseg.png' if not args.is_nnunet else '_TEM_segnn.png'
     ads_suffix = '_TEM_seg-myelin.png'
     gt_suffix = '_TEM_seg-myelin-manual.png'
+    target_model_type = 'SAM' if not args.is_nnunet else 'NNUNET'
+    target_suffix = SUFFIX_MAPPING[target_model_type][segtype]
+    ads_suffix = SUFFIX_MAPPING['ADS'][segtype]
+    gt_suffix = SUFFIX_MAPPING['BIDS'][segtype]
     
     metric = DiceMetric()
     metric2 = MeanIoU()
