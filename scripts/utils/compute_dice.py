4bf346786c68aa877c3a90c27d565fbf38f2f137
Armand Collin <armac@videotron.ca>
2023-11-16 16:21:12 -0500

    Add script args to compute_dice and allow nnUNet predictions

--- scripts/utils/compute_dice.py
+++ scripts/utils/compute_dice.py
@@ -1,25 +1,41 @@
 from monai.metrics import DiceMetric, MeanIoU
+from pathlib import Path
 import numpy as np
 import cv2
 import torch
+import argparse
+
+def get_parser():
+    # parse command line arguments
+    parser = argparse.ArgumentParser(description='Segment images using nnUNet')
+    parser.add_argument('--path-dataset', type=str, help='Path to original (BIDS) dataset.')
+    parser.add_argument('--path-preds', type=str, help='Path to predictions.')
+    parser.add_argument('--path-baseline-preds', type=str, help='Path to baseline IVADOMED predictions.')
+    parser.add_argument('--is-nnunet', default=False, action='store_true', help='if preds come from nnUNetv2 model')
+
+    return parser
 
 def main():
+    parser = get_parser()
+    args = parser.parse_args()
 
-    gt_dir = '/home/herman/Documents/NEUROPOLY_21/datasets/data_axondeepseg_tem/derivatives/labels/sub-nyuMouse26/micr/sub-nyuMouse26_sample-000'
-    fname_base = 'test_images/sub-nyuMouse26_sample-000'
-    sam_suffix = '_TEM_axonseg.png'
-    ads_suffix = '_TEM_seg-axon.png'
-    gt_suffix = '_TEM_seg-axon-manual.png'
+    gt_base = str(Path(args.path_dataset) / 'derivatives' / 'labels' / 'sub-nyuMouse26' / 'micr' / 'sub-nyuMouse26_sample-000')
+    print(gt_base)
+    fname_base = str(Path(args.path_preds) / 'sub-nyuMouse26_sample-000')
+    ads_fname_base = str(Path(args.path_baseline_preds) / 'sub-nyuMouse26_sample-000')
+    target_suffix = '_TEM_myelinseg.png' if not args.is_nnunet else '_TEM_segnn.png'
+    ads_suffix = '_TEM_seg-myelin.png'
+    gt_suffix = '_TEM_seg-myelin-manual.png'
     
     metric = DiceMetric()
     metric2 = MeanIoU()
     mean_sam, mean_ads = 0, 0
     mean2_sam, mean2_ads = 0, 0
-    print('         SAM               IVADOMED')
+    print('         TARGET            BASELINE (IVADOMED)')
     for sample in range(1, 9):
-        sam_fname = fname_base + str(sample) + sam_suffix
-        ads_fname = fname_base + str(sample) + ads_suffix
-        gt_fname = gt_dir + str(sample) + gt_suffix
+        sam_fname = fname_base + str(sample) + target_suffix
+        ads_fname = ads_fname_base + str(sample) + ads_suffix
+        gt_fname = gt_base + str(sample) + gt_suffix
 
         pred = cv2.imread(sam_fname).transpose(2,0,1) // 255
         ads = cv2.imread(ads_fname).transpose(2,0,1) // 255
@@ -36,10 +52,10 @@ def main():
         mean2_ads += ads_score2
         print('DICE: ', sam_score.item(), ads_score.item())
         print('IoU:  ', sam_score2.item(), ads_score2.item())
-    print(f'SAM MEAN DICE: {mean_sam / 8}')
-    print(f'ADS MEAN DICE: {mean_ads / 8}')
-    print(f'SAM MEAN IoU: {mean2_sam / 8}')
-    print(f'ADS MEAN IoU: {mean2_ads / 8}')    
+    print(f'TARGET MEAN DICE: {mean_sam / 8}')
+    print(f'BASELINE MEAN DICE: {mean_ads / 8}')
+    print(f'TARGET MEAN IoU: {mean2_sam / 8}')
+    print(f'BASELINE MEAN IoU: {mean2_ads / 8}')    
 
 
 if __name__ == '__main__':
